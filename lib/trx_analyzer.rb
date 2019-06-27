82fd58d8063c549889f884625d99bd352a852f55
furuhama <furuhama@users.noreply.github.com>
2019-06-28 01:15:29 +0900

    Implement prototype of monkey patch codes

--- lib/trx_analyzer.rb
+++ lib/trx_analyzer.rb
@@ -1,9 +1,14 @@
 # fronzen_string_literal: true
 
 require 'trx_analyzer/version'
+require 'trx_analyzer/transaction_manager'
+require 'trx_analyzer/abstract_connection_adapter'
 
 module TrxAnalyzer
-  def sample
-    'Hello'
-  end
+  # TODO: Initialize method
+end
+
+ActiveSupport.on_load(:active_record) do
+  ActiveRecord::ConnectionAdapters::TransactionManager.prepend TrxAnalyzer::TransactionManagerPatch
+  ActiveRecord::ConnectionAdapters::AbstractAdapter.prepend TrxAnalyzer::AbstractConnectionAdapterPatch
 end
