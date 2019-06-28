4e6218619ba0b63b0b738d4a8f89f7d4570901f1
furuhama <furuhama@users.noreply.github.com>
2019-06-28 14:13:14 +0900

    Update init! method

--- lib/sql_analyzer.rb
+++ lib/sql_analyzer.rb
@@ -7,10 +7,16 @@ require 'sql_analyzer/transaction/abstract_connection_adapter'
 module SqlAnalyzer
   # Enable monkey patching: you should call this method on application initialize phase.
   class << self
-    def init!
+    def init!(mode:)
+      raise 'mode should be :transaction OR :join' unless mode.in? %i[transaction join]
+
       ActiveSupport.on_load(:active_record) do
-        ActiveRecord::ConnectionAdapters::TransactionManager.prepend SqlAnalyzer::Transaction::TransactionManagerPatch
-        ActiveRecord::ConnectionAdapters::AbstractAdapter.prepend SqlAnalyzer::Transaction::AbstractConnectionAdapterPatch
+        if mode == :transaction
+          ActiveRecord::ConnectionAdapters::TransactionManager.prepend SqlAnalyzer::Transaction::TransactionManagerPatch
+          ActiveRecord::ConnectionAdapters::AbstractAdapter.prepend SqlAnalyzer::Transaction::AbstractConnectionAdapterPatch
+        else
+          ActiveRecord::ConnectionAdapters::AbstractAdapter.prepend SqlAnalyzer::Join::AbstractConnectionAdapterPatch
+        end
       end
     end
   end
